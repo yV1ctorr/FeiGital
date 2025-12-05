0fb7e072f0c4681ca3ef29a15dc8792b4dc42457
ryanVitor08 <ryanvitor2608@gmail.com>
2025-12-05 02:26:41 -0300

    CRUD pedido, qrcode, dashboard, outros

--- feigitalApp/forms.py
+++ feigitalApp/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from .models import Produto
+from .models import Produto, Pedido, User
 from django.contrib.auth.forms import UserCreationForm
+
 
 class ProdutoForm(forms.ModelForm):
     class Meta:
         model = Produto
         fields = ['nome', 'preco', 'validade', 'banca', 'img']
+
+
+class PedidoForm(forms.ModelForm):
+    class Meta:
+        model = Pedido
+        fields = ['status']
+
+
+class CadastroForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
